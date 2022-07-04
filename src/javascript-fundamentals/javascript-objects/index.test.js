cfe0bee65ecfa7a3169b19553b52c490aacca57e
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-04 11:00:53 -0300

    Using the describe method to separate test scenarios

--- src/javascript-fundamentals/javascript-objects/index.test.js
+++ src/javascript-fundamentals/javascript-objects/index.test.js
@@ -5,24 +5,26 @@ const {
   checkPropertyExists
 } = require('./')
 
-test('should return the value of the property with key country', () => {
-  expect(propertyKeyCountry({ continent: 'Asia', country: 'Japan' })).toBe('Japan')
-  expect(propertyKeyCountry({ country: 'Sweden', continent: 'Europe' })).toBe('Sweden')
-})
+describe('Javascript Objects', () => {
+  test('should return the value of the property with key country', () => {
+    expect(propertyKeyCountry({ continent: 'Asia', country: 'Japan' })).toBe('Japan')
+    expect(propertyKeyCountry({ country: 'Sweden', continent: 'Europe' })).toBe('Sweden')
+  })
 
-test('must create an object as a property', () => {
-  expect(createObjectOne('a')).toStrictEqual({ key: 'a' })
-  expect(createObjectOne('z')).toStrictEqual({ key: 'z' })
-  expect(createObjectOne('b')).toStrictEqual({ key: 'b' })
-})
+  test('must create an object as a property', () => {
+    expect(createObjectOne('a')).toStrictEqual({ key: 'a' })
+    expect(createObjectOne('z')).toStrictEqual({ key: 'z' })
+    expect(createObjectOne('b')).toStrictEqual({ key: 'b' })
+  })
 
-test('It should return the value of the property with key equal to the value of the string', () => {
-  expect(accessingObjByPropertie({ continent: 'Asia', country: 'Japan' }, 'continent')).toBe('Asia')
-  expect(accessingObjByPropertie({ country: 'Sweden', continent: 'Europe' }, 'country')).toBe('Sweden')
-})
+  test('It should return the value of the property with key equal to the value of the string', () => {
+    expect(accessingObjByPropertie({ continent: 'Asia', country: 'Japan' }, 'continent')).toBe('Asia')
+    expect(accessingObjByPropertie({ country: 'Sweden', continent: 'Europe' }, 'country')).toBe('Sweden')
+  })
 
-test('must check if property exists in object', () => {
-  expect(checkPropertyExists({ a: 1, b: 2, c: 3 }, 'b')).toBe(true)
-  expect(checkPropertyExists({ x: 'a', y: 'b', z: 'c' }, 'a')).toBe(false)
-  expect(checkPropertyExists({ x: 'a', y: 'b', z: 'c' }, 'z')).toBe(true)
+  test('must check if property exists in object', () => {
+    expect(checkPropertyExists({ a: 1, b: 2, c: 3 }, 'b')).toBe(true)
+    expect(checkPropertyExists({ x: 'a', y: 'b', z: 'c' }, 'a')).toBe(false)
+    expect(checkPropertyExists({ x: 'a', y: 'b', z: 'c' }, 'z')).toBe(true)
+  })
 })
--- src/javascript-fundamentals/javascript-sets/index.test.js
+++ src/javascript-fundamentals/javascript-sets/index.test.js
@@ -1,8 +1,10 @@
 const { checkValueInSet } = require('./')
 
-test('must check if value is present in Set', () => {
-  expect(checkValueInSet(new Set([1, 2, 3]), 2)).toBe(true)
-  expect(checkValueInSet(new Set([123]), 2)).toBe(false)
-  expect(checkValueInSet(new Set(['1', '2', '3']), '2')).toBe(true)
-  expect(checkValueInSet(new Set('123'), '2')).toBe(true)
+describe('Javascript Sets', () => {
+  test('must check if value is present in Set', () => {
+    expect(checkValueInSet(new Set([1, 2, 3]), 2)).toBe(true)
+    expect(checkValueInSet(new Set([123]), 2)).toBe(false)
+    expect(checkValueInSet(new Set(['1', '2', '3']), '2')).toBe(true)
+    expect(checkValueInSet(new Set('123'), '2')).toBe(true)
+  })
 })
