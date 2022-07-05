b6e2995039ec78b07e9de7df73a90ee40431418b
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-05 12:30:53 -0300

    Adjusted comments

--- src/javascript-fundamentals/javascript-objects/index.js
+++ src/javascript-fundamentals/javascript-objects/index.js
@@ -1,6 +1,9 @@
-/** Accessing object properties one
- * Write a function that takes an object with two properties as argument
- * It should return the value of the property with key country
+/** **Accessing object properties one**
+ * is a function that takes an object with two properties as argument
+ * it should return the value of the property with key country.
+ *
+ * @param {*} obj is an object
+ * @returns string
  */
 
 function propertyKeyCountry (obj) {
@@ -9,10 +12,10 @@ function propertyKeyCountry (obj) {
 
 /** **Creating Javascript objects one**
  * is a function that a string `a` as argument and
- * create an object that has a property with key 'key' and a value of `a`
+ * create an object that has a property with key 'key' and a value of `a`.
 
  * @param {*} a is a object
- * @returns the object
+ * @returns object
  */
 
 function createObjectOne (a) {
@@ -20,11 +23,13 @@ function createObjectOne (a) {
 }
 
 /** **Accessing object properties three**
- * is a function that takes an object with two properties and a string as arguments
+ * is a function that takes an object with two properties and a string as arguments.
+ *
+ * Return the value of the property with key equal to the value of the string.
  *
  * @param {*} obj is an object
- * @param {*} key is a string argument
- * @returns the value of the property with key equal to the value of the string
+ * @param {*} key is a string
+ * @returns string
  */
 
 function accessingObjByPropertie (obj, key) {
@@ -33,6 +38,7 @@ function accessingObjByPropertie (obj, key) {
 
 /** **Check if property exists in object**
  * is a function that takes an object `a` and a string `b` as argument.
+ *
  * Return true if a has `a` property with key `b`
  * and return false otherwise.
  *
@@ -46,7 +52,7 @@ function checkPropertyExists (a, b) {
 }
 
 /** **Creating Javascript objects two**
- *  is a function that takes two strings (`a` and `b`) as arguments
+ * is a function that takes two strings (`a` and `b`) as arguments
  * and create an object that has a property with key `a` and a value of `b`.
  *
  * Return the object
