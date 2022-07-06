8fa79c8c812f953d461e7b8b4968c198e5f4ef8c
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-06 10:49:18 -0300

    Refactored comments in Javascript Set

--- src/javascript-fundamentals/javascript-sets/index.js
+++ src/javascript-fundamentals/javascript-sets/index.js
@@ -1,38 +1,43 @@
-/** **Check if value is present in Set**
+/**
+ * **Check if value is present in Set**
  * is a function that takes a `Set` and a value as arguments
  * and check if the value is present in the `Set`.
  *
- * @param {*} set is an Set
- * @param {*} val is an string
- * @returns boolean
+ * @param {Set} set is an Set
+ * @param {string} val is an string
+ * @returns {boolean} the value is present in the `Set`.
  */
+
 function checkValueInSet (set, val) {
   return set.has(val)
 }
 
-/** **Convert a Set to Array**
+/**
+ * **Convert a Set to Array**
  * is a function that takes a `Set` as argument
  * and convert the `Set` to an `Array`.
  *
  * Return the Array.
  *
- * @param {*} set is an Set
- * @returns Array
+ * @param {Set} set is an Set.
+ * @returns {Array<number | string>} the `Set` converted to an `Array`.
  */
+
 function convertSetToArray (set) {
   return [...set]
 }
 
-/** **Creating Javascript Sets**
+/**
+ * **Creating Javascript Sets**
  * is a function that takes three elements of any type as arguments
  * and create a Set from those elements.
  *
  * Return the result.
  *
- * @param {*} a is an any
- * @param {*} b is an any
- * @param {*} c is an any
- * @returns Set<any>
+ * @param {any} a is an any.
+ * @param {any} b is an any.
+ * @param {any} c is an any.
+ * @returns {Set<any>} the `Set` from those elements.
  */
 
 function createSets (a, b, c) {
