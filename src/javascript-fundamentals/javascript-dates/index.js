b18a840bf0146b85dd4e5934f4b9c13a79cd4af5
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-06 10:56:31 -0300

    Refactored comments in Javascript Dates

--- src/javascript-fundamentals/javascript-dates/index.js
+++ src/javascript-fundamentals/javascript-dates/index.js
@@ -1,25 +1,27 @@
-/** **Check if one date is earlier than another**
+/**
+ * **Check if one date is earlier than another**
  * is a function that takes two date instances (`a` and `b`) as arguments
  * and It should return `true` if `a` is earlier than `b`
  * otherwise it should return `false`.
  *
- * @param {*} a is an Date
- * @param {*} b is an Date
- * @returns boolean
+ * @param {Date} a is an Date
+ * @param {Date} b is an Date
+ * @returns {boolean} if the date a earlier the date b.
  */
 
 function checkDate (a, b) {
   return a.getMinutes() < b.getMinutes() // Other solution: return a < b
 }
 
-/** **Check if two dates are equal**
+/**
+ * **Check if two dates are equal**
  * is a function that takes two date instances as arguments:
  * - It should return true if the dates are equal.
  * - It should return false otherwise
  *
- * @param {*} a is an Date
- * @param {*} b is an Date
- * @returns boolean
+ * @param {Date} a is an Date
+ * @param {Date} b is an Date
+ * @returns {boolean} if the dates are equal.
  */
 
 function checkDatesAreEqual (a, b) {
